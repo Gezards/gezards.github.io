07d4becf5ee21edb30077b2add123003c7a21a5f
Gezards <42499668+Gezards@users.noreply.github.com>
2020-08-30 23:41:55 -0300

    Update sigma.parseJson.js

--- js/sigma/sigma.parseJson.js
+++ js/sigma/sigma.parseJson.js
@@ -8,13 +8,8 @@ sigma.publicPrototype.parseJson = function(jsonPath,callback) {
 		for (i=0; i<data.nodes.length; i++){
 			var id=data.nodes[i].id;
 			//window.NODE = data.nodes[i];//In the original, but not sure purpose
-			//sigmaInstance.addNode(id,data.nodes[i]);
+			sigmaInstance.addNode(id,data.nodes[i]);
 			
-			//inverting graph y axis
-			node = data.nodes[i];
-			node.y = (node.y)*(-1); 
-
-			sigmaInstance.addNode(id,node);
 		}
 
 		for(j=0; j<data.edges.length; j++){
